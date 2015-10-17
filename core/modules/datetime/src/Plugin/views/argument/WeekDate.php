37654ce9e5520d21eb8805a5262432d27914d194
Clemens Tolboom <clemens@build2be.com>
2015-10-17 15:51:51 +0200

    Initial week argument.

---
+++ core/modules/datetime/src/Plugin/views/argument/WeekDate.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\datetime\Plugin\views\argument\DayDate.
+ */
+
+namespace Drupal\datetime\Plugin\views\argument;
+
+/**
+ * Argument handler for a day.
+ *
+ * @ViewsArgument("datetime_week")
+ */
+class WeekDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'W';
+
+}
