6a5618177bb0044c327873e4d5252ac669f86bc6
Clemens Tolboom <clemens@build2be.com>
2015-10-18 18:25:13 +0200

    Added year_month

---
+++ core/modules/datetime/src/Plugin/views/argument/YearMonthDate.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\datetime\Plugin\views\argument\DayDate.
+ */
+
+namespace Drupal\datetime\Plugin\views\argument;
+
+/**
+ * Argument handler for a day.
+ *
+ * @ViewsArgument("datetime_year_month")
+ */
+class YearMonthDate extends Date {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $argFormat = 'ym';
+
+}
